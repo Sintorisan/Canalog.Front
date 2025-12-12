558c31c59d9df10ab17d82c0fc06bbde01b012d3
Sindri Elfarsson <sindrielfarsson.dev@gmail.com>
2025-12-12 08:52:18 +0100

    name changes

--- src/app/router/todayEvents.route.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoute } from "@tanstack/react-router";
-import { rootRoute } from "./rootRoute";
-import { TodayEventsPage } from "../../pages/TodayEventsPage";
-
-export const todayRoute = createRoute({
-    getParentRoute: () => rootRoute,
-    path: "/",
-    component: TodayEventsPage,
-});
